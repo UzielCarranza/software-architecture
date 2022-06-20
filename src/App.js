fe16191da46cfc4bdbabebb03b1719352d0e9d0e
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-19 22:04:48 -0500

    created routers for server side rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import './App.css';
+import React from 'react';
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
+import {Articles} from "./pages/Articles";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <h1>Server-side rendering example</h1>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/articles">Articles</Link>
+                </li>
+            </ul>
+
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/articles" element={<Articles/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
